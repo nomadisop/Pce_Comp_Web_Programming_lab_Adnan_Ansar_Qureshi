b04b01e95e650904a1cfde0f28e7c14a3098dd1d
ROHITH PRADEEP <kvrohith56@gmail.com>
2024-03-24 16:37:25 +0530

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,10 +1,9 @@
 <?php
-$server="localhost";
-$username="root";
-$password="";
-$database="pr1";
-
     try {
+        $server="localhost";
+        $username="root";
+        $password="";
+        $database="pr1";    
         $conn = new mysqli($server,$username,$password,$database);
     
         if ($conn->connect_error) {
